07828ef722d19214a11bb05755bf8aba819791cb
Leif Gensert <leif@leif.io>
2018-06-20 20:39:33 +0100

    add basic tests for fetching image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,11 +1,15 @@
 class ImagesController < ApplicationController
-  before_action :set_image, only: [:show]
   before_action :load_image_service
 
   def create
     upload_params = params["image"]
     @image = @image_service.store_image(upload_params.to_io.path, upload_params.original_filename, upload_params.content_type)
     render :show, json: @image
+  end
+
+  def show
+    @image = @image_service.fetch_image(params["id"])
+    send_data @image.data, type: 'image/jpeg', disposition: 'inline'
   end
 
   private
--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -2,46 +2,49 @@ require 'rails_helper'
 
 RSpec.describe ImagesController, type: :controller do
 
-  let(:fixture) do
-    file_fixture('images/portrait.jpg')
+  before do
+    controller.instance_variable_set(:@image_service, image_service)
   end
 
-  let(:image_id) do
-    2711
-  end
-
-  let(:content_type) do
-    'image/jpeg'
-  end
-
-  let(:filename) do
-    fixture.basename.to_s
-  end
-
-  let(:filesize) do
-    fixture.size
-  end
-
-  let(:expected_image) do
-    Image.new(id: image_id, content_type: content_type, filename: filename, filesize: filesize)
-  end
-
-  let(:image_service) do
-    spy("ImageService", store_image: expected_image)
+  after do
+    controller.instance_variable_set(:@image_service, nil)
   end
 
   describe "POST #create" do
+    let(:fixture) do
+      file_fixture('images/portrait.jpg')
+    end
+
+    let(:image_id) do
+      2711
+    end
+
+    let(:content_type) do
+      'image/jpeg'
+    end
+
+    let(:filename) do
+      fixture.basename.to_s
+    end
+
+    let(:filesize) do
+      fixture.size
+    end
+
+    let(:expected_image) do
+      Image.new(id: image_id, content_type: content_type, filename: filename, filesize: filesize)
+    end
+
+    let(:image_service) do
+      spy("ImageService", store_image: expected_image)
+    end
+
+    before do
+      request.headers.merge!({'ACCEPT' => 'application/json'})
+      post :create, params: {image: fixture_file_upload(fixture.to_path, content_type)}
+    end
+
     context "with valid params" do
-      before do
-        controller.instance_variable_set(:@image_service, image_service)
-        request.headers.merge!({'ACCEPT' => 'application/json'})
-        post :create, params: {image: fixture_file_upload(fixture.to_path, content_type)}
-      end
-
-      after do
-        controller.instance_variable_set(:@image_service, nil)
-      end
-
       it "returns JSON response with 200 OK" do
         expect(response).to have_http_status(:ok)
         expect(response.content_type).to eq('application/json')
@@ -61,6 +64,55 @@ RSpec.describe ImagesController, type: :controller do
       it "calls ImageService" do
         expect(image_service).to have_received(:store_image).with(a_string_ending_with('.jpg'), filename, content_type)
       end
+    end
+  end
+
+  describe "GET #show" do
+    let(:fixture) do
+      file_fixture('images/portrait.jpg')
+    end
+
+    let(:image_id) do
+      2711
+    end
+
+    let(:content_type) do
+      'image/jpeg'
+    end
+
+    let(:filename) do
+      fixture.basename.to_s
+    end
+
+    let(:filesize) do
+      fixture.size
+    end
+
+    let(:expected_image) do
+      Image.new(
+        id: image_id,
+        content_type: content_type,
+        filename: filename,
+        filesize: filesize,
+        data: fixture.read,
+      )
+    end
+
+    let(:image_service) do
+      spy("ImageService", fetch_image: expected_image)
+    end
+
+    before do
+      get :show, params: {id: "#{image_id}.jpg"}
+    end
+
+    it "returns jpg response with 200 OK" do
+      expect(response).to have_http_status(:ok)
+      expect(response.content_type).to eq('image/jpeg')
+    end
+
+    it "returns body of image" do
+      expect(response.body).to eq(fixture.read)
     end
   end
 end
