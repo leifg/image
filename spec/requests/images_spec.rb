fe5f7e52cf6e888c302c53347ee0b1029c9d7435
Leif Gensert <leif@leif.io>
2018-06-20 20:17:28 +0100

    add integration test

--- spec/requests/images_spec.rb
+++ spec/requests/images_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Images", type: :request do
-  describe "GET /images" do
-    xit "works! (now write some real specs)" do
-      get image_path(1)
+  describe "POST /images" do
+    let(:fixture) do
+      file_fixture('images/portrait.jpg')
+    end
+
+    let(:content_type) do
+      "image/jpeg"
+    end
+
+    it "returns 200 OK" do
+      post images_path, params: {image: fixture_file_upload(fixture.to_path, content_type)}
       expect(response).to have_http_status(200)
+    end
+
+    it "writes to database" do
+      expect {
+        post images_path, params: {image: fixture_file_upload(fixture.to_path, content_type)}
+      }.to change{Image.count}.by(1)
     end
   end
 end
